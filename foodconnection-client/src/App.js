8a7a721eb5670ec15bbd5748738d29c4e26587e0
Vanessa Denardin <vanessa.denardin@gmail.com>
2021-07-17 16:43:38 +1000

    created react app for client; set up components - WIP

---
+++ foodconnection-client/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      home
+    </div>
+  );
+}
+
+export default App;
---
+++ foodconnection-client/src/components/RecipeList.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class RecipeList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  
+})
+
+const mapDispatchToProps = {
+  
+}
+
+export default RecipeList
