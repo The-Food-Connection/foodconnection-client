64b9b9e118fcb22dbbbeeba66a8df5b585e87556
Vanessa Denardin <vanessa.denardin@gmail.com>
2021-07-18 19:29:57 +1000

    initial set up RecipeList component showing all recipes

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,21 +1,49 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Card, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 
-export class RecipeList extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+
+export default function RecipeList() {
+
+  const [recipes, setRecipes] = useState([]);
+
+  // const [update, setUpdate] = useState(false);
+
+  const fetchRecipes = async () => {
+    const response = await fetch(process.env.REACT_APP_API_URL + "/recipes", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    const data = await response.json();
+    console.log(data)
+    setRecipes(data);
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={recipe.image} />
+          <Card.Body>
+            <Card.Title>{recipe.recipe_name}</Card.Title>
+            <Card.Text>
+              {recipe.cuisine}
+            </Card.Text>
+            <Card.Text>
+              {recipe.meal_type}
+            </Card.Text>
+            <Button variant="primary">Learn more</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => ({
-  
-})
-
-const mapDispatchToProps = {
-  
-}
-
-export default RecipeList
