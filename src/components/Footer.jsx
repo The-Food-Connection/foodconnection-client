4b4a68301720abe2a7467d49fff97944324d9c43
suztopp <suzannetopp81@gmail.com>
2021-07-18 13:15:11 +1000

    added navbar and footer code back in to repo, added some colour styling, changed links to be inline with our plan

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,46 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import fclogo from "../images/FClogo.JPG";
 
-export class Footer extends Component {
-  render() {
-    return (
-      <div>
-        
+const Footer = () => {
+  return (
+    <MDBFooter color="mdb-color darken-1" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            {/* <h5 className="title">Footer Content</h5> */}
+            <img src={fclogo} width="300px"/>
+            {/* <p>
+              Here you can use rows and columns here to organize your footer
+              content.
+            </p> */}
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">ALL RECIPES</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!">My Profile</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">My Recipes</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Add Recipe</a>
+              </li>
+              {/* <li className="list-unstyled">
+                <a href="#!">ABOUT US</a>
+              </li> */}
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          {/* &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a> */} 
+        </MDBContainer>
       </div>
-    )
-  }
+    </MDBFooter>
+  );
 }
 
-
-export default Footer
+export default Footer;
