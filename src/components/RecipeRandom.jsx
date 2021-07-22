da7e88c2b17824c9b917704768a92c5c928a1c6d
Vanessa Denardin <vanessa.denardin@gmail.com>
2021-07-22 11:54:54 +1000

    recipe random in progress

--- src/components/RecipeRandom.jsx
+++ src/components/RecipeRandom.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
+import RecipeList from './RecipeList';
 import './styling/RecipeRandom.css';
 
 export default function RecipeRandom() {
 
-    const [random, setRandom] = useState({});
+    // // const [randomRecipe, setRandomRecipe] = useState({});
 
+    // // const getRandomRecipe = async (randomRecipe) => {
+    // //     return randomRecipe[Math.floor(Math.random() * recipes.length)]
+    // //     // data.recipe.randomRecipe = data.randomRecipe
+    // // }
+
+    // // useEffect(() => {
+    // //     randomRecipe(getRandomRecipe);
+    // // }, []);
+
+    // const [getRandomRecipe, setRandomRecipe] = useState({});
+
+    // const fetchRandomRecipe = async () => {
+    //     const randomRecipe = Math.floor(Math.random() * recipes.length)
+    //     const response = await fetch(`${process.env.REACT_APP_API_URL}/recipes/${randomRecipe}`, {
+    //         headers: {
+    //             Authorization: `Bearer ${localStorage.getItem("token")}`,
+    //         }
+    //     });
+
+    //     const data = await response.json();
+    //     console.log(data)
+    //     // to keep just one state, I am adding the image url to the recipe object
+    //     data.randomRecipe.recipe = data.randomRecipe
+    //     setRandomRecipe(data.randomRecipe.recipe);
+    // };
+
+    // useEffect(() => {
+    //     fetchRandomRecipe();
+    // }, [randomRecipe])
     return (
         <div>
             <Card className="text-center">
@@ -14,7 +44,7 @@ export default function RecipeRandom() {
                 <Card.Body>
                     <Card.Title>Feel Like Spicing Up Dinner?</Card.Title>
                     <Card.Text>
-                    Our Creators have picked something amazing for you to cook today, check it out!
+                        Our Creators have picked something amazing for you to cook today, check it out!
                     </Card.Text>
                     <Button variant="primary">VIEW RECIPE</Button>
                 </Card.Body>
